5291f92cf5e1ec866aff00b312a479aedb3361eb
Sunny17082 <91121970+Sunny17082@users.noreply.github.com>
2023-07-10 13:10:59 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import HighLightIcon from "@material-ui/icons/Highlight";
+
+
+function Header() {
+    return <header>
+        <h1><HighLightIcon />Keeper</h1>
+    </header>;
+}
+
+export default Header;
